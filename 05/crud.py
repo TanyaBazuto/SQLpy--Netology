09f763ebe96d5e5dfa0ddce73335da3f37f8c987
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-02-11 10:09:22 +0300

    Update crud.py

--- 05/crud.py
+++ 05/crud.py
@@ -3,7 +3,7 @@ import psycopg2
 
 with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
     with conn.cursor() as cur:
-        # удаление таблиц
+        # удаление таблиц (в начале для возможности удалить все данные и заново наполнять)
         # cur.execute("""
         # DROP TABLE homework;
         # DROP TABLE course;
@@ -28,14 +28,14 @@ with psycopg2.connect(database="netology_db", user="postgres", password="postgre
 
         # наполнение таблиц (C из CRUD)
         cur.execute("""
-        INSERT INTO course(name) VALUES('Python');
+        INSERT INTO course(name) VALUES('Python');   #SERIAL ставит id автоматически?
         """)
         conn.commit()  # фиксируем в БД
 
         cur.execute("""
-        INSERT INTO course(name) VALUES('Java') RETURNING id;
+        INSERT INTO course(name) VALUES('Java') RETURNING id;       #результат наполнения не виден сразу для просмотра RETURUNG. Например, вернуть id для значения 'Java' в  столбце name  таблицы course
         """)
-        print(cur.fetchone())  # запрос данных автоматически зафиксирует изменения
+        print(cur.fetchone())  # запрос данных автоматически зафиксирует изменения = conn.commit()
 
         cur.execute("""
         INSERT INTO homework(number, description, course_id) VALUES(1, 'простое дз', 1);
@@ -61,12 +61,12 @@ with psycopg2.connect(database="netology_db", user="postgres", password="postgre
         cur.execute("""
         SELECT name FROM course;
         """)
-        print(cur.fetchall())
+        print(cur.fetchall())     #извлекает список кортежей
 
         cur.execute("""
         SELECT id FROM course WHERE name='Python';
         """)
-        print(cur.fetchone())
+        print(cur.fetchone())       #извлекает один кортеж(!не список значений, а один объект - кортеж)
 
         cur.execute("""
         SELECT id FROM course WHERE name='{}';
@@ -74,18 +74,18 @@ with psycopg2.connect(database="netology_db", user="postgres", password="postgre
         print(cur.fetchone())
 
         cur.execute("""
-        SELECT id FROM course WHERE name=%s;
-        """, ("Python",))  # хорошо, обратите внимание на кортеж
+        SELECT id FROM course WHERE name=%s;           #%s(элемент заполнитель) - рлдстановка вместо параметров - для безопасной передачи
+        """, ("Python",))  # хорошо, обратите внимание на кортеж, т.е. если передаем один параметр, то в скобках и после ставим ","
         print(cur.fetchone())
 
 
         -- ФУНКЦИЯ для извлечения данных: узнать идентификатор курса по его имени. С её помощью передаем str и получаем id.
         def get_course_id(cursor, name: str) -> int:
             cursor.execute("""
-            SELECT id FROM course WHERE name=%s;
-            """, (name,))
-            return cur.fetchone()[0]
-        python_id = get_course_id(cur, 'Python')
+            SELECT id FROM course WHERE name=%s;              #передаем запрос с подстановкой %s(элемент заполнитель)
+            """, (name,))                                     #вторым аргументом передаем кортеж-параметр
+            return cur.fetchone()[0]                          #хотим получить запись и достать нулевой элемент, т.к. возвращается кортеж объект.
+        python_id = get_course_id(cur, 'Python')              #использование функции: вызываем функцию и передаем текущий курсор(который может выполнять SQLзапросы) и параметр для выполнения
         print('python_id', python_id)
 
         cur.execute("""
@@ -94,14 +94,14 @@ with psycopg2.connect(database="netology_db", user="postgres", password="postgre
         conn.commit()  # фиксируем в БД
 
         cur.execute("""
-        SELECT * FROM homework;
+        SELECT * FROM homework;         #если не укажем конкретные столбцы, которые хотим получить из таблицы и поставим *
         """)
-        print(cur.fetchall())
+        print(cur.fetchall())           #то вернет список всех кортежей со всеми столбцами
 
         # обновление данных (U из CRUD)
         cur.execute("""
-        UPDATE course SET name=%s WHERE id=%s;
-        """, ('Python Advanced', python_id))
+        UPDATE course SET name=%s WHERE id=%s;    # %s - подстановка вместо параметров, для безопасной передачи параметров
+        """, ('Python Advanced', python_id))      # передача котрежей параметров
         cur.execute("""
         SELECT * FROM course;
         """)
